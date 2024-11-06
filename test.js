3bd5a17bd5275ffaf27c68907a773bdf103f05ba
Lucas Eduardo Zamora Sulub <lucasosulub2@gmail.com>
2024-11-06 13:40:41 -0500

    Testeo en preproduccion
    Se realizaron las pruebas requeridas en preproducción para la simulación

---
+++ test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+describe("Hola Mundo", () => {
+    it("Debe imprimir Hola, Mundo", () => {
+        const output = "Hola, Mundo";
+        assert.strictEqual(output, "Hola, Mundo");
+    });
+});
